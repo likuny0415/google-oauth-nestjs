5d38f58cf24e1d0a7a57c8b10be46b777b032912
likuny0415 <li.kuny@husky.neu.edu>
2022-03-10 17:57:20 +0800

    test

--- src/google.strategy.ts
+++ src/google.strategy.ts
@@ -13,7 +13,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy) {
     super({
       clientID: "1084312825929-efefge1ampu1nkkvb6upq1lqrtleka8h.apps.googleusercontent.com",
       clientSecret: "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",
-      callbackURL: 'http://localhost:3000/google/redirect',
+      callbackURL: 'https://kuny-todo.herokuapp.com/google/redirect',
       scope: ['email', 'profile'],
     });
   }
